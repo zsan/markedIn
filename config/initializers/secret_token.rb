e650df880280327f36e10adc3e0fb7e35a6719e9
zsan <zaksantanu@gmail.com>
2013-06-01 13:37:32 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CitygridAdv::Application.config.secret_token = 'df69ef01d26728981258d50a320296a93f3838ab1e22d1e32af6037e460ff025c4c58d2517caf33a1e0fe5ad4c3bdc199d2a9407bc6aba2b25f3749d71b2aa95'
