8d7f9ba50c7a88fcf5e6cd8b2405b9989cf47ec7
zsan <zaksantanu@gmail.com>
2013-06-08 03:23:49 +0700

    finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,5 @@
 CitygridAdv::Application.configure do
+  require 'bcrypt'
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
@@ -34,4 +35,9 @@ CitygridAdv::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering BCrypt's cost function
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 CitygridAdv::Application.routes.draw do
-  get "users/new"
-
+  resources :users
 	root to: 'static_pages#home'
 
   match '/signup', to: "users#new"
